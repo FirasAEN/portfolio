5f53ded1f6a970f1ecae0543df4dab27ce32ec19
Firas ABED EL NABI <firas.abedelnabi@adobisgroup.fr>
2025-08-31 02:46:30 +0200

    feat: various adaptations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
 
+
+const isDev = import.meta.env.DEV;
+
+let base = isDev ? '.' : '/portfolio';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://firasaen.github.io', // Use to generate your sitemap and canonical URLs in your final build.
+    base,
+    trailingSlash: 'ignore', // Use to always append '/' at end of url
+});
